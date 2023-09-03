ac67f3335b73f9a72b12aacf20bee3802e5e0c02
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-09-03 10:27:33 +0100

    Task 04

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,7 @@
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+const { ObjectId } = require('mongodb');
 
 const sha1 = require('sha1');
 
@@ -20,6 +23,23 @@ class UserController {
     const sha1Password = sha1(password);
     queryResult = await users.insertOne({ email, password: sha1Password });
     return res.status(201).send({ id: queryResult.insertedId, email });
+  }
+
+  static async getMe(req, res) {
+    // retreive token from request headers
+    const token = req.headers['x-token'];
+    if (!token) return res.status(401).send({ error: 'Unauthorized' });
+
+    // retreive token from redis
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) return res.status(401).send({ error: 'Unauthorized' });
+
+    // retreive user from database
+    const users = await dbClient.db.collection('users');
+    const user = await users.findOne({ _id: ObjectId(userId) });
+    if (!user) return res.status(401).send({ error: 'Unauthorized' });
+
+    return res.send({ id: user._id, email: user.email });
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import AppContoller from '../controllers/AppController';
 import UserController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const express = require('express');
 
@@ -8,5 +9,8 @@ const router = express.Router();
 router.get('/status', AppContoller.getStatus);
 router.get('/stats', AppContoller.getStats);
 router.post('/users', UserController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UserController.getMe);
 
 export default router;
